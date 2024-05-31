eff32a6896297dadd12aea4956fb9ea1ba4ab299
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2024-06-01 00:13:51 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Example8RegistrationForm
-Example8RegistrationForm
+**Introduction about the code:**
+
+Below link explains the steps by which this code is generated.
+
+https://coursecs.wordpress.com/courses/advance-web-development/sending-data-from-registration-form-to-database-using-endpoint-backend-project/
+
+Below video link for explanation of this code.
+
+Link will be available soon. Video is under process.
+
+**How to Run:**
+
+- Open VS code terminal and type command
+
+      npm install
+
+- Above command will install all neccessary packages and create node_modules folder in your downloaded code.
+
+- Now run below command to run the code
+
+      npm run dev
---
+++ src/component/Register.jsx
@@ -0,0 +1,234 @@
+// File: Register.jsx located in root-directory/src/component/
+
+import React from "react";
+import { useState } from "react";
+
+function Register() {
+  //initialize states for all input fields
+  const [allInputData, setInputData] = useState({
+    name: "",
+    fname: "",
+    email: "",
+    password: "",
+    cpassword: "",
+    status: "",
+  });
+  const onChangeInput = (event) => {
+    //name of variable "event" can be changed
+    setInputData({ ...allInputData, [event.target.name]: event.target.value });
+  };
+
+  const savingData = async (
+    name,
+    fname,
+    email,
+    password,
+    cpassword,
+    status
+  ) => {
+    const myEndpoint = "http://localhost:5000/user/register-user/";
+    try {
+      const response = await fetch(myEndpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          fname,
+          email,
+          password,
+          cpassword,
+          status,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const newUser = {
+        name: name,
+        fname: fname,
+        email: email,
+        password: password,
+        status: status,
+      };
+      const myJsonData = await response.json();
+      console.log("myJsonData: ", myJsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    }
+  };
+
+  const onSubmitHandle = (event) => {
+    event.preventDefault(); //prevent page loading when click on button
+
+    savingData(
+      allInputData.name,
+      allInputData.fname,
+      allInputData.email,
+      allInputData.password,
+      allInputData.cpassword,
+      allInputData.status
+    );
+
+    //setting all input fields blank
+    setInputData({
+      name: "",
+      fname: "",
+      email: "",
+      password: "",
+      cpassword: "",
+      status: "",
+    });
+  };
+
+  return (
+    <div>
+      {/* Registration Page Container */}
+      <div className="p-5">
+        <div className="text-center m-10">
+          <h2 className="text-3xl font-bold dark:text-white">Registration</h2>
+        </div>
+        <div className="text-center">
+          <p>Register yourself to use our services.</p>
+        </div>
+        <div>
+          <section className="">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:m-2 lg:py-0">
+              <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <form action="#" className="space-y-4 md:space-y-6">
+                    <div>
+                      <label
+                        htmlFor="name"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Full Name
+                      </label>
+                      <input
+                        name="name"
+                        id="name"
+                        required=""
+                        value={allInputData.name}
+                        onChange={onChangeInput}
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="M Abo Bakar Aslam"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="fname"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Father Name
+                      </label>
+                      <input
+                        name="fname"
+                        id="fname"
+                        required=""
+                        value={allInputData.fname}
+                        onChange={onChangeInput}
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="M Aslam"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="email"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Your email
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        required=""
+                        value={allInputData.email}
+                        onChange={onChangeInput}
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="name@company.com"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="password"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="••••••••"
+                        required=""
+                        value={allInputData.password}
+                        onChange={onChangeInput}
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="confirm-password"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Confirm password
+                      </label>
+                      <input
+                        type="password"
+                        name="cpassword"
+                        id="cpassword"
+                        placeholder="••••••••"
+                        required=""
+                        value={allInputData.cpassword}
+                        onChange={onChangeInput}
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      />
+                    </div>
+                    <div>
+                      <label
+                        htmlFor="status"
+                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Status
+                      </label>
+                      <input
+                        name="status"
+                        id="status"
+                        required=""
+                        value={allInputData.status}
+                        onChange={onChangeInput}
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Teacher/Student"
+                      />
+                    </div>
+
+                    <button
+                      type="submit"
+                      onClick={onSubmitHandle}
+                      className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                    >
+                      Create an Account
+                    </button>
+                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Already have an account?
+                      <a
+                        href="#"
+                        className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                      >
+                        Login here
+                      </a>
+                    </p>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
